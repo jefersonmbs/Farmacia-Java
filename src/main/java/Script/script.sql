a74d867b076178c7b5fa807613d70bb591293450
Jeferson Martins <jeferson.santos@ilog.com.br>
2019-05-05 16:00:46 -0300

    Update Busca Cliente

---
+++ src/main/java/Script/script.sql
@@ -0,0 +1,41 @@
+#
+# TABLE STRUCTURE FOR: cliente
+#
+
+
+CREATE TABLE `cliente` IF IF NOT EXISTS (
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  UNIQUE KEY `cpf_2` (`cpf`),
+  KEY `id` (`id`),
+  KEY `cpf` (`cpf`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+INSERT INTO `cliente` ( `nome`, `cpf`) VALUES ( 'Kennedy Blanda', '10781284003');
+INSERT INTO `cliente` ( `nome`, `cpf`) VALUES ('Micaela Kassulke', '15631831019');
+INSERT INTO `cliente` ( `nome`, `cpf`) VALUES ('Emilie Denesik', '18855196750');
+INSERT INTO `cliente` ( `nome`, `cpf`) VALUES ( 'Jackeline White', '32803258440');
+INSERT INTO `cliente` ( `nome`, `cpf`) VALUES ( 'Sabrina Schmitt V', '37662620191');
+INSERT INTO `cliente` ( `nome`, `cpf`) VALUES ( 'Prof. Olen Kautzer Jr.', '57084893016');
+INSERT INTO `cliente` ( `nome`, `cpf`) VALUES ( 'Ansley Reichel', '62710092449');
+INSERT INTO `cliente` ( `nome`, `cpf`) VALUES ( 'Bettye Schowalter', '67736990610');
+INSERT INTO `cliente` ( `nome`, `cpf`) VALUES ( 'Dr. Hellen Conn', '73253778554');
+INSERT INTO `cliente` ( `nome`, `cpf`) VALUES ( 'Rahsaan Cole PhD', '92757064430');
+
+
+#
+# TABLE STRUCTURE FOR: produto
+#
+
+DROP TABLE IF EXISTS `produto`;
+
+CREATE TABLE `produto` (
+  `idProduto` varchar(10) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `preco` decimal(10,2) unsigned NOT NULL,
+  `estoqueAtual` int(11) NOT NULL,
+  `estoqueMinimo` int(11) NOT NULL,
+  KEY `idProduto` (`idProduto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
--- src/main/java/executavel/Executavel.java
+++ src/main/java/executavel/Executavel.java
@@ -2,10 +2,12 @@ package executavel;
 
 import view.Menu;
 
+import java.util.Locale;
+
 public class Executavel {
 
 	public static void main(String[] args) {
-		
+		Locale.setDefault(Locale.US);
 		Menu menu = new Menu();
 		menu.apresentaMenu();
 		
